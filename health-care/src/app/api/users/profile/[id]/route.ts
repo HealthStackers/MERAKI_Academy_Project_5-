931f90b5ea0e6d7b587532a0790b2908c63412fe
MarwahHatamleh <marwahatamleh98@gmail.com>
2025-06-10 22:48:46 +0300

    Created folder structure

---
+++ health-care/src/app/api/users/profile/[id]/route.ts
@@ -0,0 +1,17 @@
+import { UpdateUser } from "@/models/lib/db/services/users";
+import { NextResponse } from "next/server";
+
+export const PUT =async (
+  request: Request,
+  params: {
+    params: Promise<{ id: string }>;
+  }
+) => {
+
+const {id} = params.params;
+
+const body = (await request.json()) as UpdateUser
+const result  =await UpdateUser(id , body)
+return NextResponse.json(result , {status : 200})
+
+};
