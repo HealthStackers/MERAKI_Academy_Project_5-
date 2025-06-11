d12fa784146ae08bb81867c68476d63ec3220b6f
MarwahHatamleh <marwahatamleh98@gmail.com>
2025-06-11 20:35:28 +0300

    added try catch to show an error

--- health-care/src/app/api/appointments/route.ts
+++ health-care/src/app/api/appointments/route.ts
@@ -1,18 +1,21 @@
 import { bookAppointment } from "@/models/lib/db/services/appointment";
 import { NextResponse } from "next/server";
 
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
 
-
-
-
-export const POST = async(request: Request) => {
-  const body = await request.json();
-
-  const result = await bookAppointment(body);
-  return NextResponse.json(result, {
-    status: 201,
-  });
-
-
-
+    const result = await bookAppointment(body);
+    return NextResponse.json(result, {
+      status: 201,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    return NextResponse.json(
+      { msg: `Failed ${error.message}` },
+      {
+        status: 404,
+      }
+    );
+  }
 };
