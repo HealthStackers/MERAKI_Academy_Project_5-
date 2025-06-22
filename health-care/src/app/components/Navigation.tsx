b018ad590a7b057ffa095549aadccc82c5a29466
BelalAbandah <belalabandah@gmail.com>
2025-06-22 23:33:40 +0300

    home page has been done, and i am working on join request page

--- health-care/src/app/components/Navigation.tsx
+++ health-care/src/app/components/Navigation.tsx
@@ -11,30 +11,32 @@ const Navigation = () => {
   const { roleId, setRoleId, userId, setUserId } = useContext(AuthContext);
 
   const [showAdminPanel, setShowAdminPanel] = useState(false);
-  console.log("roleId: ",roleId);
- 
-  const token= localStorage.getItem("token")
-   console.log("token:",token);
-
- // const [token, settoken] = useState(localStorage.getItem("token") || "null" );
+  const [showJoinRequest, setShowJoinRequest] = useState(false);
+  console.log("roleId: ", roleId);
+  const [token, settoken] = useState(localStorage.getItem("token") || null);
 
   useEffect(() => {
     const roleId = localStorage.getItem("roleId");
     if (roleId !== null) {
       if (+roleId === 1) {
         setShowAdminPanel(true);
-      } else if (+roleId === 2 || +roleId === 3) {
+        setShowJoinRequest(false);
+      } else if (+roleId === 2) {
         setShowAdminPanel(false);
+        setShowJoinRequest(true);
+      } else if (+roleId === 3) {
+        setShowAdminPanel(false);
+        setShowJoinRequest(false);
       }
     }
   }, []);
 
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div className="container-fluid">
           <button
-            class="btn btn-outline-secondary d-lg-none"
+            className="btn btn-outline-secondary d-lg-none"
             type="button"
             data-bs-toggle="offcanvas"
             data-bs-target="#mobileDrawer"
@@ -46,81 +48,82 @@ const Navigation = () => {
             <span id="letter">M</span>ed<span id="letter">i</span>vo
           </a>
 
-          <div class="collapse navbar-collapse desktop-menu">
-            <ul class="navbar-nav">
+          <div className="collapse navbar-collapse desktop-menu">
+            <ul className="navbar-nav">
               <div className="partOneInNavBar">
-                <li class="nav-item active">
-                  <a class="nav-link" href="home">
+                <li className="nav-item active">
+                  <a className="nav-link" href="home">
                     Home
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="blogs">
+                <li className="nav-item">
+                  <a className="nav-link" href="blogs">
                     Blogs
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="symptomChecker">
+                <li className="nav-item">
+                  <a className="nav-link" href="symptomChecker">
                     Symptom Checker
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="bmiCalculator">
+                <li className="nav-item">
+                  <a className="nav-link" href="bmiCalculator">
                     BMI
                   </a>
                 </li>
-                <li class="nav-item dropdown">
+                <li className="nav-item dropdown">
                   <a
-                    class="nav-link dropdown-toggle"
+                    className="nav-link dropdown-toggle"
                     href="#"
                     role="button"
                     data-bs-toggle="dropdown"
                   >
                     Services
                   </a>
-                  <ul class="dropdown-menu">
+                  <ul className="dropdown-menu">
                     <li>
-                      <a class="dropdown-item" href="#">
+                      <a className="dropdown-item" href="#">
                         Action
                       </a>
                     </li>
                     <li>
-                      <a class="dropdown-item" href="#">
+                      <a className="dropdown-item" href="#">
                         Another action
                       </a>
                     </li>
                     <li>
-                      <a class="dropdown-item" href="#">
+                      <a className="dropdown-item" href="#">
                         Something else
                       </a>
                     </li>
                   </ul>
                 </li>
                 {showAdminPanel && (
-                  <li class="nav-item">
-                    <a class="nav-link" href="adminPanel">
+                  <li className="nav-item">
+                    <a className="nav-link" href="adminPanel">
                       Admin Panel
                     </a>
                   </li>
                 )}
-
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Contact
-                  </a>
-                </li>
+                {showJoinRequest && (
+                  <li className="nav-item">
+                    <a className="nav-link" href="joinRequest">
+                      Join Request
+                    </a>
+                  </li>
+                )}
               </div>
               {!token && (
                 <div className="partTwoInNavBar">
                   <li className="loginAndRegister">
-                    <li class="nav-item">
+                    <li className="nav-item">
                       {" "}
-                      <a href="login" class="nav-link">
+                      <a href="login" className="nav-link">
                         <button id="loginButtonInNavBar">Login</button>
                       </a>
                     </li>
-                    <li class="nav-item">
-                      <a href="register" class="nav-link">
+                    <li className="nav-item">
+                      <a href="register" className="nav-link">
                         <button id="registerButtonInNavBar">Register</button>
                       </a>
                     </li>
@@ -132,54 +135,54 @@ const Navigation = () => {
         </div>
       </nav>
 
-      <div class="offcanvas offcanvas-start" tabindex="-1" id="mobileDrawer">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title">Menu</h5>
+      <div className="offcanvas offcanvas-start" id="mobileDrawer">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title">Menu</h5>
           <button
             type="button"
-            class="btn-close"
+            className="btn-close"
             data-bs-dismiss="offcanvas"
           ></button>
         </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
+        <div className="offcanvas-body">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
                 Features
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
                 Pricing
               </a>
             </li>
-            <li class="nav-item dropdown">
+            <li className="nav-item dropdown">
               <a
-                class="nav-link dropdown-toggle"
+                className="nav-link dropdown-toggle"
                 href="#"
                 role="button"
                 data-bs-toggle="dropdown"
               >
                 Dropdown link
               </a>
-              <ul class="dropdown-menu">
+              <ul className="dropdown-menu">
                 <li>
-                  <a class="dropdown-item" href="#">
+                  <a className="dropdown-item" href="#">
                     Action
                   </a>
                 </li>
                 <li>
-                  <a class="dropdown-item" href="#">
+                  <a className="dropdown-item" href="#">
                     Another action
                   </a>
                 </li>
                 <li>
-                  <a class="dropdown-item" href="#">
+                  <a className="dropdown-item" href="#">
                     Something else
                   </a>
                 </li>
